d0580d8a85a57e02c9b64589f2c53f30166e961e
Paul Cox <paulcox24@gmail.com>
2014-10-23 16:16:48 -0600

    Add carousel to profile

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,5 @@
 class Attachment < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600x600>" }
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
